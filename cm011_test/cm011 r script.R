b30782ad58d87debd543d9bd287b814b6d5cbcdc
Lauren Smith <lauren.j.smith34@gmail.com>
2019-10-08 10:38:26 -0700

    commit cm 011

--- cm011_test/cm011 r script.R
+++ cm011_test/cm011 r script.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(gapminder)
+library(here)
 
 gapminder
 write_csv(gapminder, './gapminder.csv') #write_csv is to create a csv file 
@@ -20,6 +21,16 @@ gapminder_sum %>%
 
 #read_csv can read from url, but read_excel
 data_url <- "http://gattonweb.uky.edu/sheather/book/docs/datasets/GreatestGivers.xls"
+file_name <- basename(data_url)
 download.file(url = data_url,
-              destfile = paste(here::here("cm011_test", "greatestGivers")))
-              
+              destfile = paste(here::here("cm011_test", file_name)))
+
+library(readxl)
+philanthropists <- read_excel(here::here("cm011_test", file_name))
+
+
+
+
+mri_file = here("datasets", "Firas-MRI.xlsx")
+mri <- read_excel(mri-file)
+
