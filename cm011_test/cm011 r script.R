ab04e1b20dd94b200b38cba34e31281beda2f819
Lauren Smith <lauren.j.smith34@gmail.com>
2019-10-08 10:45:28 -0700

    cm011

--- cm011_test/cm011 r script.R
+++ cm011_test/cm011 r script.R
@@ -31,6 +31,19 @@ philanthropists <- read_excel(here::here("cm011_test", file_name))
 
 
 
-mri_file = here("datasets", "Firas-MRI.xlsx")
-mri <- read_excel(mri-file)
+mri_file = here("cm011_test", "Firas-MRI.xlsx")
+mri <- read_excel(mri_file)
+mri
+mri <- mri[,-10]
+
+mri <- mri %>%
+  pivot_longer(cols = "Slice 1":"Slice 8", 
+               names_to = "slice_no",
+               values_to = "value")
+view(mri)
+
+
+
+
+
 
