ca84e90869e3a09f2dbf8fc3772159488e737404
Lauren Smith <lauren.j.smith34@gmail.com>
2019-10-08 09:54:56 -0700

    commit

--- cm011 r script.R
+++ cm011 r script.R
@@ -4,3 +4,15 @@ library(gapminder)
 gapminder
 write_csv(gapminder, './gapminder.csv') #write_csv is to create a csv file 
 view(gapminder)
+
+# calculate mean life expectancy
+gapminder_sum <- gapminder %>%
+  group_by(continent) %>%
+  summarize(ave_life = mean(lifeExp))
+view(gapminder_sum)
+write_csv(gapminder_sum, './gapminder_sum.csv')
+
+gapminder_sum %>%
+  ggplot(aes(continent, ave_life)) +
+  geom_point() +
+  theme_bw()
